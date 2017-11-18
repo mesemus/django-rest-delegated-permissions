8035dbfa966263a8926a44a48fbb1f4fb6303778
Mirek Simek <miroslav.simek@vscht.cz>
2017-11-18 19:36:09 +0100

    speeding up tests a bit

--- tests/test_item_base.py
+++ tests/test_item_base.py
@@ -62,34 +62,35 @@ class BaseTestItemB:
     # noinspection PyAttributeOutsideInit
     @pytest.fixture(autouse=True)
     def environ(self):
-        self.containers = []
-        self.items = []
-        self.guardian_items = []
-
         non_rights_container = Container.objects.create(name='non_rights')
 
-        for i in range(5):
-            container = Container.objects.create(name='container_%s' % i)
-            for j in range(2):
-                item = ItemB.objects.create(name='ItemB_%s_%s' % (i, j))
+        self.items = []
+        self.guardian_items = []
+        self.guardian_directly_on_items = []
+        self.containers = []
+        self.guardian_containers = []
+
+        for guardian_container in (0, 1):
+            container = Container.objects.create(name='container_%s' % guardian_container)
+            self.containers.append(container)
+            if guardian_container:
+                self.guardian_containers.append(container)
+            for guardian_directly_on_item in (0, 1):
+                item = ItemB.objects.create(name='ItemB_%s_%s' %
+                                                 (guardian_container, guardian_directly_on_item))
                 item.parents.add(container)
                 item.parents.add(non_rights_container)
                 self.items.append(item)
-                if i < 2:
+                if guardian_container:
                     self.guardian_items.append(item)
-
-            self.containers.append(container)
-
-        self.guardian_containers = self.containers[:2]
+                if guardian_directly_on_item:
+                    self.guardian_directly_on_items.append(item)
 
         self.view_permission = Permission.objects.get(codename='view_container')
         self.change_permission = Permission.objects.get(codename='change_container')
 
         self.view_item_permission = Permission.objects.get(codename='view_itemb')
         self.change_item_permission = Permission.objects.get(codename='change_itemb')
-
-        # take 1/4 of items in A for guardian on items
-        self.guardian_directly_on_items = random.sample(self.items, len(self.items)//4)
 
     @pytest.fixture()
     def item_class(self):
@@ -114,19 +115,19 @@ class BaseTestItemC:
         self.containers = []
         self.items = []
         self.guardian_items = []
+        self.guardian_containers = []
 
         Container.objects.create(name='no_item_c')
 
-        for i in range(5):
+        for i in range(4):
             item = ItemC.objects.create(name='ItemC_%s' % (i,))
             container = Container.objects.create(name='container_%s' % i, item_c=item)
             self.items.append(item)
+            self.containers.append(container)
             if i < 2:
                 self.guardian_items.append(item)
+                self.guardian_containers.append(container)
 
-            self.containers.append(container)
-
-        self.guardian_containers = self.containers[:2]
 
         self.view_permission = Permission.objects.get(codename='view_container')
         self.change_permission = Permission.objects.get(codename='change_container')
@@ -134,8 +135,10 @@ class BaseTestItemC:
         self.view_item_permission = Permission.objects.get(codename='view_itemc')
         self.change_item_permission = Permission.objects.get(codename='change_itemc')
 
-        # take 1/4 of items in A for guardian on items
-        self.guardian_directly_on_items = random.sample(self.items, len(self.items)//4)
+        self.guardian_directly_on_items = [
+            self.items[0],
+            self.items[2]
+        ]
 
     @pytest.fixture()
     def item_class(self):
@@ -157,34 +160,35 @@ class BaseTestItemD(BaseTestItemQuerySets, BaseUsers):
     # noinspection PyAttributeOutsideInit
     @pytest.fixture(autouse=True)
     def environ(self):
-        self.containers = []
-        self.items = []
-        self.guardian_items = []
-
         non_rights_container = Container.objects.create(name='non_rights')
 
-        for i in range(5):
-            container = Container.objects.create(name='container_%s' % i)
-            for j in range(2):
-                item = ItemD.objects.create(name='ItemD_%s_%s' % (i, j))
+        self.items = []
+        self.guardian_items = []
+        self.guardian_directly_on_items = []
+        self.containers = []
+        self.guardian_containers = []
+
+        for guardian_container in (0, 1):
+            container = Container.objects.create(name='container_%s' % guardian_container)
+            self.containers.append(container)
+            if guardian_container:
+                self.guardian_containers.append(container)
+            for guardian_directly_on_item in (0, 1):
+                item = ItemD.objects.create(name='ItemD_%s_%s' %
+                                                 (guardian_container, guardian_directly_on_item))
                 item.containers.add(container)
                 item.containers.add(non_rights_container)
                 self.items.append(item)
-                if i < 2:
+                if guardian_container:
                     self.guardian_items.append(item)
-
-            self.containers.append(container)
-
-        self.guardian_containers = self.containers[:2]
+                if guardian_directly_on_item:
+                    self.guardian_directly_on_items.append(item)
 
         self.view_permission = Permission.objects.get(codename='view_container')
         self.change_permission = Permission.objects.get(codename='change_container')
 
         self.view_item_permission = Permission.objects.get(codename='view_itemd')
         self.change_item_permission = Permission.objects.get(codename='change_itemd')
-
-        # take 1/4 of items in A for guardian on items
-        self.guardian_directly_on_items = random.sample(self.items, len(self.items)//4)
 
     @pytest.fixture()
     def item_class(self):
