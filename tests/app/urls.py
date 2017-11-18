3f8e4a2810590480394a2d5e65a65a0ef41cbb52
Mirek Simek <miroslav.simek@vscht.cz>
2017-11-18 17:52:02 +0100

    urls for rest items

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import ContainerViewSet, ItemAViewSet
+from .viewsets import ContainerViewSet, ItemAViewSet, ItemBViewSet, ItemCViewSet, ItemDViewSet
 
 router = DefaultRouter()
 router.register(r'container', ContainerViewSet)
 router.register(r'item/A', ItemAViewSet)
+router.register(r'item/B', ItemBViewSet)
+router.register(r'item/C', ItemCViewSet)
+router.register(r'item/D', ItemDViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
