c171bd941848a2d149fe7180e42ff2cbc77e66a6
Mirek Simek <miroslav.simek@vscht.cz>
2017-11-20 17:40:35 +0100

    publishing to pip

--- setup.py
+++ setup.py
@@ -1,14 +1,43 @@
-import os
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = """
+Delegate django rest framework object permissions to an object pointed by foreign key/m2m
+
+Sample: set up permissions so that anyone having django/django guardian permissions on Invoice will have the same set of permissions on its address:
+
+.. code-block:: python
+
+    class Address(models.Model):
+        ... address fields
+
+    class Invoice(models.Model):
+        address = models.OneToOneField(Address, related_name='invoice')
+        ... invoice fields
+
+    perms = RestPermissions()
+
+    @perms.apply(permissions=DelegatedPermissions(perms, "invoice")
+    class AddressViewSet(ModelViewSet):
+        queryset = Address.objects.all()
+        serializer = AddressSerializer
+        ...
+
+    @perms.apply()            # implicitely adds django model permissions and guardian permissions
+    class InvoiceViewSet(ModelViewSet):
+        queryset = Invoice.objects.all()
+        serializer = InvoiceSerializer
+        ...
+        
+See docs and API at `github <https://github.com/mesemus/django-rest-delegated-permissions>`_.
+"""
 
 
 setup(
     name='django-rest-delegated-permissions',
-    version='0.1',
-    packages=['rest_delegated_permissions'],
+    version='0.5.5',
+    packages=[
+        'rest_delegated_permissions',
+    ],
     description='Taking (delegating) REST permissions from a model instance to a model pointed by ForeignKey/m2m',
     long_description=README,
     author='Mirek Simek',
@@ -20,14 +49,14 @@ setup(
         'djangorestframework',
         'rest_condition',
         'django-guardian',
-        'pytest-runner',
-        'pytest-env'
     ],
     tests_require=[
         'tox',
         'pytest',
         'pytest-django',
-        'pytest_matrix'
+        'pytest_matrix',
+        'pytest-runner',
+        'pytest-env'
     ],
     extras_require={
         'dev': [
